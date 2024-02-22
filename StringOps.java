302721e0ab60c35da93516e2ae0937a12700abd7
romigoodman <romi.goodman@post.runi.ac.il>
2024-02-22 17:45:20 +0200

    first

--- StringOps.java
+++ StringOps.java
@@ -22,12 +22,52 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+     String myWord= "Hello World";
+     System.out.println(capVowelsLowRest(myWord));
     }
 
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+    public static String lowerCase(String string) 
+
+    {
+        String conversToLowerCase = "";
+
+        for (int i=0; i<string.length(); i++) 
+        {
+            char isUpperCase = string.charAt(i);
+            
+            if ((isUpperCase>= 'A') && (isUpperCase<='Z'))         
+            {
+            conversToLowerCase = conversToLowerCase + (char)(string.charAt(i) + 32); 
+            }
+
+            else
+            {
+                conversToLowerCase = conversToLowerCase + isUpperCase;
+            }
+
+        }
+        return conversToLowerCase;
+    }
+  
+    public static String capVowelsLowRest (String string) 
+    {
+        String answerFinal =  lowerCase(string); //function i wrote in HW 3 that puts everything in lowercase
+        String toBePrinted = "";
+
+        for (int i=0; i < answerFinal.length(); i++) 
+        {
+            char ch = answerFinal.charAt(i);
+
+            if ((ch == 'i' ) || (ch == 'o' ) || ( ch == 'a' ) || ( ch == 'u' ) || ( ch == 'e' )) 
+            {
+            toBePrinted = toBePrinted + (char)(answerFinal.charAt(i) - 32);   
+            }
+            else 
+            {
+            toBePrinted = toBePrinted + ch;
+            }
+        }
+        return toBePrinted;
     }
 
     public static String camelCase (String string) {
